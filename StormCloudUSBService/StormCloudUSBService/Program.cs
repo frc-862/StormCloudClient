de2e33f9919a52fbffad129510a0287ae4031929
David Reeves <djreeves2005@gmail.com>
2023-01-05 00:48:07 -0500

    Actual console service?

---
+++ StormCloudUSBService/StormCloudUSBService/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace StormCloudUSBService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureServices((c, s) =>
+                {
+                    s.AddHostedService<USBManager>();
+                })
+                .Build()
+                .Run();
+            
+        }
+    }
+}
---
+++ StormCloudUSBService/StormCloudUSBService/USBManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StormCloudUSBService
+{
+    public class USBManager : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            USBListener l = new USBListener();
+            l.BeginListening();
+            while (!cancellationToken.IsCancellationRequested)
+            {
+
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
